b212232307e5539ce9d25d86520a897523b3170d
hojat <fardinho@msu.edu>
2020-10-29 15:03:49 -0400

    parental confirmation for minors

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -7,18 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  maxDate;
-  minDate;
+  maxDate: Date;
+  minDate: Date;
+  now : Date = new Date();
+  isMinor : boolean = false;
+  next : boolean = false;
+  message: string = "";
+  @Input() birthDate: Date;
+
 
   constructor() { }
 
   ngOnInit(): void {
-
     this.maxDate = new Date();
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 14)
     this.minDate = new Date;
     this.minDate.setFullYear(this.minDate.getFullYear() - 60)
+  }
 
+  onSubmit(f : NgForm) {
+
+  }
+
+  onNext(f : NgForm) {
+    const birthdate : Date = f.controls.bdate.value;
+    this.isMinor = this.now.getFullYear() - this.birthDate.getFullYear() < 18;
+    this.message = this.isMinor ? "Parent's email" : "Your email";
+    this.next = true;
   }
 
 }
